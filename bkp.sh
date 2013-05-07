776774fec279301d2ac8a07b65630207c4a7ddec
mribeirodantas@lais.huol.ufrn.br <mribeirodantas@lais.huol.ufrn.br>
2013-05-06 21:13:30 -0300

    Fixed bug when there was only one dir to back up

--- bkp.sh
+++ bkp.sh
@@ -9,14 +9,15 @@ else
 fi
 
 # Number of folders to back up
-n=10
 
-for i in `seq 1 $n`
+echo `cat /tmp/pacu/backupLine | cut -d "\"" -f2` > /tmp/pacu/backupLine
+n=`cat /tmp/pacu/backupLine | awk '-F[ ]' '{ t += NF - 1 } END { print t }'`
+for i in `seq 0 $n`
 do
-	dir=`cat /tmp/backupLine  | cut -d " " -f$((i+1))`
+	dir=`cat /tmp/pacu/backupLine  | cut -d " " -f$((i+1))`
 	if [ "$dir" != "" ];
 	then
-		file=`echo $dir | gawk -F "\"" '{ print $1 }'`
+		file=`echo $dir | gawk -F " " '{ print $1 }'`
 		#perform not-so-far-chosen backup strategy over the file below
 		echo $file
 	fi
