0a3aba581ed0c613b51149c767af2ab4ed21b409
mribeirodantas@lais.huol.ufrn.br <mribeirodantas@lais.huol.ufrn.br>
2013-05-13 21:04:18 -0300

    Replaced tabspaces for spaces

--- install.sh
+++ install.sh
@@ -2,27 +2,27 @@
 #################################################################
 #   PACU 0.1
 #       PACS Automated Computer Utilities
-#    _____          _       _____   ______   
-#   |_   _|        / \     |_   _|.' ____ \  
-#     | |         / _ \      | |  | (___ \_| 
-#     | |   _    / ___ \     | |   _.____`.  
-#    _| |__/ | _/ /   \ \_  _| |_ | \____) | 
-#   |________||____| |____||_____| \______.' 
+#    _____          _       _____   ______
+#   |_   _|        / \     |_   _|.' ____ \
+#     | |         / _ \      | |  | (___ \_|
+#     | |   _    / ___ \     | |   _.____`.
+#    _| |__/ | _/ /   \ \_  _| |_ | \____) |
+#   |________||____| |____||_____| \______.'
 #           Copyright (c) 2013
 #   <mribeirodantas at lais.huol.ufrn.br>
-#   
+#
 # This program is free software; you can redistribute it and/or
 # modify it under the terms of the GNU General Public License
 # as published by the Free Software Foundation; either version 2
 # of the License, or (at your option) any later version.
-# 
+#
 # This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
 #
 # Read further in the LICENSE file.
-#   
+#
 #################################################################
 
 # Variable
@@ -31,7 +31,7 @@ ARCH="noarch"
 
 if [ "$SUDO_USER" == "" ];
 then
-	SUDO_USER="`who -m | awk '{print $1;}'`"
+  SUDO_USER="`who -m | awk '{print $1;}'`"
 fi
 
 LOGPATH="/home/$SUDO_USER/.pacuLog"
@@ -40,97 +40,97 @@ LOGPATH="/home/$SUDO_USER/.pacuLog"
 
 function log()
 {
-	echo [  `date --date=now +%r`] $1 >> $LOGPATH
-	if [ "$2" == "print" ];
-	then
-		echo -e "$1"
-	fi
+  echo [  `date --date=now +%r`] $1 >> $LOGPATH
+  if [ "$2" == "print" ];
+  then
+    echo -e "$1"
+  fi
 }
 function install()
 {
-	clear
-	echo "pacu.$VERSION-$ARCH"
-	echo "Thank you for using the PACS Automated Computer Utilities!"
-	echo "You may want to take a look at the RELEASE file in order"
-	echo "to see what has changed since the last version!"
-	echo -e "\nWe suggest you to read the README file, before"
-	echo "going on. Do you want to continue?"
-	read -n1 -p [Y/n] answer
-	if [ "$answer" != "y" ] && [ "$answer" != "Y" ];
-	then
-		echo -e "\n"
-		exit 0
-	else
-		echo -e "\n"
-	fi
-	# Creating Installation Log file
-	date > $LOGPATH || (echo -e "Installation could not continue since it was unable to create log file\n" && exit 0)
-	echo >> $LOGPATH
-	uname -a >> $LOGPATH
-	echo >> $LOGPATH
-	lsb_release -a >> $LOGPATH
-	echo >> $LOGPATH
+  clear
+  echo "pacu.$VERSION-$ARCH"
+  echo "Thank you for using the PACS Automated Computer Utilities!"
+  echo "You may want to take a look at the RELEASE file in order"
+  echo "to see what has changed since the last version!"
+  echo -e "\nWe suggest you to read the README file, before"
+  echo "going on. Do you want to continue?"
+  read -n1 -p [Y/n] answer
+  if [ "$answer" != "y" ] && [ "$answer" != "Y" ];
+  then
+    echo -e "\n"
+    exit 0
+  else
+    echo -e "\n"
+  fi
+  # Creating Installation Log file
+  date > $LOGPATH || (echo -e "Installation could not continue since it was unable to create log file\n" && exit 0)
+  echo >> $LOGPATH
+  uname -a >> $LOGPATH
+  echo >> $LOGPATH
+  lsb_release -a >> $LOGPATH
+  echo >> $LOGPATH
 
-	log "[PACU installation started]" "print"
-	log "`echo Creating pacu binary..`" "print"
-	sleep 1
-	log "`cp -rf pacu.sh /usr/local/bin/pacu.sh && ln -fs /usr/local/bin/pacu.sh /usr/local/bin/pacu 2>&1`"
-	log "`echo Installing freedups..`" "print"
-	sleep 1
-	log "`cp -rf third-party/freedups.sh /usr/local/bin/freedups.sh && ln -fs /usr/local/bin/freedups.sh /usr/local/bin/freedups 2>&1`"
-	log "Creating configuration files in /home/$SUDO_USER/.." "print"
-	sleep 1
-	if [ -f /home/$SUDO_USER/.pacu ];
-	then
-		echo "There is already a configuration file in /home/$SUDO_USER/"
-		echo "Overwrite it?"
-		read -n1 -p "[y/N]" answer
-		if [ "$answer" != "n" ] && [ "$answer" != "N" ];
-		then
-			log "`cp -rf .pacu /home/$SUDO_USER/ 2>&1`"
-			echo -e "\n"
-			log "Configuration file overwritten." "print"
-			sleep 1
-		else
-			echo -e "\n"
-			exit 0
-		fi
-	else
-		log "`cp -rf .pacu /home/$SUDO_USER/ 2>&1`"
-	fi
-	log "Checking installation.." "print"
-	sleep 1
-	if [ -f /home/$SUDO_USER/.pacu ] && [ -f /usr/local/bin/pacu ];
-	then
-		log "PACU was successfully installed." "print"
-		echo "You can view the installation log file in"
-		echo "/home/$SUDO_USER/.pacuLog"
-	else
-		echo "For some reason, pacu was not installed."
-		echo "Check the log for further information."
-	fi
+  log "[PACU installation started]" "print"
+  log "`echo Creating pacu binary..`" "print"
+  sleep 1
+  log "`cp -rf pacu.sh /usr/local/bin/pacu.sh && ln -fs /usr/local/bin/pacu.sh /usr/local/bin/pacu 2>&1`"
+  log "`echo Installing freedups..`" "print"
+  sleep 1
+  log "`cp -rf third-party/freedups.sh /usr/local/bin/freedups.sh && ln -fs /usr/local/bin/freedups.sh /usr/local/bin/freedups 2>&1`"
+  log "Creating configuration files in /home/$SUDO_USER/.." "print"
+  sleep 1
+  if [ -f /home/$SUDO_USER/.pacu ];
+  then
+    echo "There is already a configuration file in /home/$SUDO_USER/"
+    echo "Overwrite it?"
+    read -n1 -p "[y/N]" answer
+    if [ "$answer" != "n" ] && [ "$answer" != "N" ];
+    then
+      log "`cp -rf .pacu /home/$SUDO_USER/ 2>&1`"
+      echo -e "\n"
+      log "Configuration file overwritten." "print"
+      sleep 1
+    else
+      echo -e "\n"
+      exit 0
+    fi
+  else
+    log "`cp -rf .pacu /home/$SUDO_USER/ 2>&1`"
+  fi
+  log "Checking installation.." "print"
+  sleep 1
+  if [ -f /home/$SUDO_USER/.pacu ] && [ -f /usr/local/bin/pacu ];
+  then
+    log "PACU was successfully installed." "print"
+    echo "You can view the installation log file in"
+    echo "/home/$SUDO_USER/.pacuLog"
+  else
+    echo "For some reason, pacu was not installed."
+    echo "Check the log for further information."
+  fi
 }
 
 # Beginning
 
 if [[ $EUID -ne 0 ]]; then
-	echo "You must be a root user to perform the installation." 2>&1
-	exit 1
+  echo "You must be a root user to perform the installation." 2>&1
+  exit 1
 else
-	if [ -f /usr/local/bin/pacu ]; then
-		echo "PACU is already installed." 2>&1
-		echo "Are you sure you want to reinstall it?" 2>&1
-		read -n1 -p "[y/N]" answer
-		case "$answer" in
-			y|Y)
-				echo ""
-				install
-				;;
-			*)
-				echo -e "\nProcess finished."
-				;;
-		esac
-	else
-		install
-	fi
+  if [ -f /usr/local/bin/pacu ]; then
+    echo "PACU is already installed." 2>&1
+    echo "Are you sure you want to reinstall it?" 2>&1
+    read -n1 -p "[y/N]" answer
+    case "$answer" in
+      y|Y)
+        echo ""
+        install
+        ;;
+      *)
+        echo -e "\nProcess finished."
+        ;;
+    esac
+  else
+    install
+  fi
 fi
--- pacu.sh
+++ pacu.sh
@@ -10,7 +10,7 @@
 #   |________||____| |____||_____| \______.' 
 #           Copyright (c) 2013
 #   <mribeirodantas at lais.huol.ufrn.br>
-#   
+#
 # This program is free software; you can redistribute it and/or
 # modify it under the terms of the GNU General Public License
 # as published by the Free Software Foundation; either version 2
@@ -30,7 +30,7 @@ PkgName=PACU_a0.1_noarch
 PkgVersion=v0.1
 if [ "$USER" == "" ];
 then
-   SUDO_USER="`who -m | awk '{print $1;}'`"
+  SUDO_USER="`who -m | awk '{print $1;}'`"
 fi
 
 
@@ -58,10 +58,10 @@ echo restore PACS db
 }
 function miss()
 {
-	echo "PACS Automated Computer Utilities" $PkgVersion
-	echo "pacu [parameter] [option] [argument]"
-	echo "Run the help for further information."
-	echo "pacu --help"
+  echo "PACS Automated Computer Utilities" $PkgVersion
+  echo "pacu [parameter] [option] [argument]"
+  echo "Run the help for further information."
+  echo "pacu --help"
 }
 
 function backup()
@@ -69,9 +69,9 @@ function backup()
 # Preparing temporary files for parsing
 if [ -d /tmp/pacu ];
 then
-   echo `grep "backup_source" /home/$USER/.pacu` > /tmp/pacu/backupLine
+  echo `grep "backup_source" /home/$USER/.pacu` > /tmp/pacu/backupLine
 else
-   mkdir /tmp/pacu && echo `grep "backup_source" /home/$USER/.pacu` > /tmp/pacu/backupLine
+  mkdir /tmp/pacu && echo `grep "backup_source" /home/$USER/.pacu` > /tmp/pacu/backupLine
 fi
 
 # Number of folders to back up
@@ -80,26 +80,26 @@ echo `cat /tmp/pacu/backupLine | cut -d "\"" -f2` > /tmp/pacu/backupLine
 n=`cat /tmp/pacu/backupLine | awk '-F[ ]' '{ t += NF - 1 } END { print t }'`
 for i in `seq 0 $n`
 do
-	dir=`cat /tmp/pacu/backupLine  | cut -d " " -f$((i+1))`
-	if [ "$dir" != "" ];
-	then
-		file=`echo $dir | gawk -F " " '{ print $1 }'`
-		echo "Backing up $file.."
-		sleep 1
-		if [ -d "$file" ];
-		then
-			if [ "$1" == "full" ];
-			then
-				tar zcvf $1-$(basename $file)-`date +%Y%m%d`.tar.gz $file
-				else if [ "$1" == "inc" ];
-				then
-					find $file -mtime -$2 -type f -print | tar zcvf $1-$(basename $file)-`date +%Y%m%d`.tar.gz -T -
-				fi
-			fi
-		else
-			echo "$file does not exist."
-		fi
-	fi
+  dir=`cat /tmp/pacu/backupLine  | cut -d " " -f$((i+1))`
+  if [ "$dir" != "" ];
+  then
+    file=`echo $dir | gawk -F " " '{ print $1 }'`
+    echo "Backing up $file.."
+    sleep 1
+    if [ -d "$file" ];
+    then
+      if [ "$1" == "full" ];
+      then
+        tar zcvf $1-$(basename $file)-`date +%Y%m%d`.tar.gz $file
+        else if [ "$1" == "inc" ];
+        then
+          find $file -mtime -$2 -type f -print | tar zcvf $1-$(basename $file)-`date +%Y%m%d`.tar.gz -T -
+        fi
+      fi
+    else
+      echo "$file does not exist."
+    fi
+  fi
 done
 tar zcf backup-$1-`date +%Y%m%d`.tar.gz --remove-files $1*-`date +%Y%m%d`.tar.gz && echo "The backup process is finished." && exit 0
 echo "There was an error and the backup process could not finish properly."
@@ -107,60 +107,61 @@ echo "There was an error and the backup process could not finish properly."
 
 function help()
 {
-	echo "PACS Automated Computer Utilities" $PkgVersion
-	echo "pacu [parameter] [option] [argument]"
-	echo -e "\nParameters\n"
-	echo "	-s --send	Send images to a PACS Server"
-	echo "	-q --query	Query images from a PACS Server"
-	echo "	-d --dump	Dump the PACS Server database"
-	echo "	-r --restore	Restore the PACS Server database"
-	echo "	--full-backup	Full backup of dcm4chee server"
-	echo "	--inc-backup [days] Incremental backup"
-	echo "	-h --help	Display this help"
-	echo -e "\nOptions\n"
+  echo "PACS Automated Computer Utilities" $PkgVersion
+  echo "pacu [parameter] [option] [argument]"
+  echo -e "\nParameters\n"
+  echo "  -s --send Send images to a PACS Server"
+  echo "  -q --query  Query images from a PACS Server"
+  echo "  -d --dump Dump the PACS Server database"
+  echo "  -r --restore  Restore the PACS Server database"
+  echo "  --full-backup Full backup of dcm4chee server"
+  echo "  --inc-backup [days] Incremental backup"
+  echo "  --freedups [options]"
+  echo "  -h --help Display this help"
+  echo -e "\nOptions\n"
 }
 
 function freedups()
 {
-	/usr/local/bin/freedups
+  /usr/local/bin/freedups
 }
 
 # Beginning
 case "$1" in
-	-s|--send)
-		send
-		;;
-	-q|--query)
-		query
-		;;
-	-p|--plot)
-		plot
-		;;
-	-d|--dump)
-		dump
-		;;
-	-r|--restore)
-		restore
-		;;
-	-h|--help)
-		help
-		;;
-	--full-backup)
-		backup full
-		;;
-	--inc-backup)
-		if [ "$2" == "" ];
-		then
-			echo "You must inform the number of days after --inc-backup."
-			exit 0
-		else
-			backup inc $2
-		fi
-		;;
-	--freedups)
-		freedups
-		;;
-	*)
-		miss
-		;;
+  -s|--send)
+    send
+    ;;
+  -q|--query)
+    query
+    ;;
+  -p|--plot)
+    plot
+    ;;
+  -d|--dump)
+    dump
+    ;;
+  -r|--restore)
+    restore
+    ;;
+  -h|--help)
+    help
+    ;;
+  --full-backup)
+    backup full
+    ;;
+  --inc-backup)
+    if [ "$2" == "" ];
+    then
+      echo "You must inform the number of days after --inc-backup."
+      exit 0
+    else
+      backup inc $2
+    fi
+    ;;
+  --freedups)
+    freedups
+    ;;
+  *)
+    miss
+    ;;
 esac
