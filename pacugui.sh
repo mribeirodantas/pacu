2c22651a625c0af92d06e7531d27e9f9012c91ea
mribeirodantas@lais.huol.ufrn.br <mribeirodantas@lais.huol.ufrn.br>
2013-05-16 23:04:29 -0300

    Added /home/mribeirodantas

--- pacugui.sh
+++ pacugui.sh
@@ -1,4 +1,29 @@
-#!/bin/bash
+#! /bin/bash
+#################################################################
+#   PACU 0.1
+#       PACS Automated Computer Utilities
+#    _____          _       _____   ______
+#   |_   _|        / \     |_   _|.' ____ \
+#     | |         / _ \      | |  | (___ \_|
+#     | |   _    / ___ \     | |   _.____`.
+#    _| |__/ | _/ /   \ \_  _| |_ | \____) |
+#   |________||____| |____||_____| \______.'
+#           Copyright (c) 2013
+#   <mribeirodantas at lais.huol.ufrn.br>
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# Read further in the LICENSE file.
+#
+#################################################################
 INPUT=/tmp/menu.sh.$$
 
 #Help
@@ -20,7 +45,7 @@ function about() {
 
 #Backup
 function backup() {
-  line=`grep backup_source /home/mribeirodantas/.pacu | awk -F\" {' print $2 '}`
+  line=`grep backup_source $HOME/.pacu | awk -F\" {' print $2 '}`
   IFS=: read -r -a array <<<"$line"
   cmd=(dialog --backtitle "PACS Automated Computer Utilities" \
               --title "Backing up nodes" --scrollbar \
